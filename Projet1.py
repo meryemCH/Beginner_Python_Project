320dc15a019ada2969d9e3f784e4dbf81be353f7
meryemCH <meryemchrigui2015@gmail.com>
2024-11-14 16:38:42 +0100

    initial commit

---
+++ Projet1.py
@@ -0,0 +1,8 @@
+import random
+
+def guess(x):
+    random_number =random.randint(1,x)
+    guess =0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}:'))
+        
